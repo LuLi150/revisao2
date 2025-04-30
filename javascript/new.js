8a8bab4f0915438c2ef90115398e6d6b62f3bf06
LuLi150 <luabranca.0987@gmail.com>
2025-04-30 17:00:55 -0300

    Add files via upload

---
+++ javascript/new.js
@@ -0,0 +1,50 @@
+//  let senha=Number(prompt("Digite sua senha: "));
+// console.log(nota);
+
+//  if (nota>=7){
+//     console.log("Aprovado");
+//  } else if (nota<5){
+//     console.log("reprovado");
+//  } else {
+//   console.log("recuperacao");
+//  }
+//--------
+// while (senha!==1234) {
+//     senha=Number(prompt("senha invalida. Tente Novamente"));
+// }
+// console.log("Acesso permitido");
+//----------
+// let opcao;
+
+// do {
+//     let nome= prompt("Qual seu nome?")
+//     opcao = Number(prompt(`
+//         Em que posso ajudar?
+//         1.Dizer Ola
+//         2. Sair`))
+//    if(opcao===1){
+//       alert(`Ola, ${nome}`)
+//     }
+// } while (opcao!==2);
+// console.log("Saiu!");
+//------
+
+//  function verificarIdade(idade) {
+//     if (idade>=18){
+//         return true;  
+//     }
+//         return false;
+//  }
+// console.log(verificarIdade(15));
+//----------
+
+    for(let numero=1;numero<=10;numero++){
+        if(numero % 2 == 0){
+           console.log(`${numero} numero par`);
+        } else {
+            console.log(`${numero} numero impar`);
+            
+        }
+    }
+
+ 
