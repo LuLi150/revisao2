eda50f537b189cdfedcf31874fa649f6b300ad25
LuLi150 <luabranca.0987@gmail.com>
2025-04-30 13:15:47 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+
+// let numero=10;
+
+// if(numero==10){
+//     console.log("numero maior");
+// } else{
+//     console.log(numero);
+// }
+
+let dataDeNascimento=1990;
+let idade=2025-dataDeNascimento;
+
+if(idade>18){
+    console.log("pode votar");
+    
+} else{
+    console.log("nao pode votar");  
+}
+console.log(idade);
